9721d099dfe93cfc7729d384049a15975305e195
amino <amino@amino.com>
2024-04-22 20:51:22 -0400

    Adding static build to release

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 rmdir /Q /S build
 mkdir build
+rmdir /Q /S lp\logs
+rmdir /Q /S lp\modules\__pycache__
+rmdir /Q /S lp\__pycache__
+rmdir /Q /S lp\core\__pycache__
 
-cmake -B build . 
+cmake -B build . -DCMAKE_BUILD_TYPE=%1
 cmake --build build 
